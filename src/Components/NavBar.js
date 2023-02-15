30d88129bb59c92eeb7bfd19e1ec40b98f295255
stella <sftamesis13@gmail.com>
2023-02-15 11:32:03 -0800

    setup Home, App, and Navbar basic code

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <nav>
+            <NavLink exact to ="/">Home</NavLink>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
