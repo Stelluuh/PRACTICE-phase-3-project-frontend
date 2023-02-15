1187d25f32c76894c280ea2f5715d19f5680bac0
stella <sftamesis13@gmail.com>
2023-02-15 13:28:19 -0800

    added introduction to Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h3>Hello World</h3>
+      <h3>BookSelf</h3>
+      <p>BookSelf, is an App that allows users to keep track of all the books they have read in their lifetime. Users are allowed to add and/or remove books from their shelf as well as give them a rating. </p>
     </div>
   )
 }
