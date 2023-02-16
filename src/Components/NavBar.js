08d0dc00e1f4383e9e001c4ed4ff6b57cea37cf3
stella <sftamesis13@gmail.com>
2023-02-16 15:28:24 -0800

    Add links to route user to different pages of the application

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 const NavBar = () => {
   return (
     <div>
-      
+      <nav>
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink to="/shelf">Shelf</NavLink>
+        <NavLink to="/shelf/add">Add Book</NavLink>
+      </nav>
     </div>
   )
 }
