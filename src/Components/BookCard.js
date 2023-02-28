c36e4f9f1b7fc6cf9cc56de7585483a572fa7e0f
stella <sftamesis13@gmail.com>
2023-02-27 16:04:31 -0800

    include bookshelf data to show Location of book

--- src/Components/BookCard.js
+++ src/Components/BookCard.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const BookCard = ({id, title, author, genre, location, read, reader}) => {
+const BookCard = ({id, title, author, genre, bookshelf}) => {
   return (
     <tr>
       <td>{title}</td>
       <td>{author}</td> 
       <td>{genre}</td>
-      <td>{location}</td>
-      <td>{read}</td>
-      <td>{reader}</td>
+      <td>{bookshelf.location}</td>
+      
       <td>
         <button type="button" class="btn btn-primary btn-sm">Edit</button>
         <button type="button" class="btn btn-secondary btn-sm">Delete</button>
