4c41d0de97388eddd7b438e1bf80018d0bab531a
stella <sftamesis13@gmail.com>
2023-03-02 14:37:46 -0800

    switch to react-router-dom version 6

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,55 +1,74 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import NewBook from './NewBook';
-import Bookshelf from './Bookshelf';
+import Bookshelves from './Bookshelves';
 import Home from './Home';
 import Readers from './Readers';
 import ReaderDetails from './ReaderDetails';
+import Shelf from './Shelf'
 
 
 const App = () => {
   const [readers, setReaders] = useState([])
+  // const [books, setBooks] = useState([])
+  const [bookshelves, setBookshelves] = useState([])
 
-    useEffect(() => {
-        fetch('http://localhost:9292/readers')
-        .then(response => response.json())
-        .then(data => setReaders(data))
-    }, [])
+// 
+
+  useEffect(() => {
+      fetch('http://localhost:9292/readers')
+      .then(response => response.json())
+      .then(data => setReaders(data))
+  }, [])
+
+
+  useEffect(() => {
+    fetch('http://localhost:9292/bookshelfs')
+      .then(response => response.json())
+      .then(data => setBookshelves(data))
+  }, [])
+
+  console.log({bookshelves})
 
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <NavBar />
 
-      <Switch>
+      <Routes>
         <Route path="/bookshelf/add">
           <NewBook />
         </Route>
+       
+        <Route 
+          exact path="/bookshelfs" element={
+            <Bookshelves 
+            key={bookshelves.id}
+            bookshelves={bookshelves}
+            setBookshelves={setBookshelves}
+            />
+          }
+        />
 
-        <Route path="/bookshelf">
-          <Bookshelf 
-            
-          />
-        </Route>
-
-        <Route exact path="/readers">
-          <Readers 
+        <Route path="/bookshelfs/:id"element={<Shelf />}/>
+          
+        <Route 
+          exact path="/readers" element={
+            <Readers
             key={readers.id}
-            readers={readers}
-            setReaders={setReaders}
-          />
-        </Route>
+              readers={readers}
+              setReaders={setReaders}
+            />
+          }
+        />
 
-        <Route path="/readers/:id">
-          <ReaderDetails />
-        </Route>
+        <Route path="/readers/:id" element={<ReaderDetails />}/>
+        <Route exact path="/readers/:id" element={<Home />}/>
         
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-
+      </Routes>
     </div>
+    </Router>
   );
 }
 
